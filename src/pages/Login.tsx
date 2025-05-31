899eb684fcaed64c8617b72a0d58c703504272cb
jyy0913 <chanel.gjf@gmail.com>
2025-06-01 02:15:18 +0900

    bug: 로그인 리다이렉션 배포용으로 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,13 +2,9 @@ import BackgroundImage from "../assets/Login-Logo.svg";
 import GoogleLogo from "../assets/G-logo.svg";
 
 const Login = () => {
-  const REDIRECT_URL = "http://localhost:5173"; 
-
   const handleLogin = () => {
-    const encoded = btoa(REDIRECT_URL);
-    window.location.href = `https://aether.asia/oauth2/authorization/google?state=${encoded}`;
+    window.location.href = "https://aether.asia/oauth2/authorization/google";
   };
-
   return (
     <div
       className="relative w-full h-screen bg-white flex items-center justify-start"
