9cc041755bcba3ad7fe462ecc459d4c892e38488
Baetato <diana021015@gmail.com>
2025-06-04 09:20:12 +0900

    hotfix: notice dummy delete

--- src/api/noticeApi.ts
+++ src/api/noticeApi.ts
@@ -1,26 +1,27 @@
 import { axiosInstance } from './lib/axios';
 
 // 공지 조회
-// export const fetchNotices = async () => {
-//   const response = await axiosInstance.get('/notices');
-//   return response.data.result.notices ?? [];
-// };
 export const fetchNotices = async () => {
-  try {
-    const response = await axiosInstance.get("/notices");
-    console.log("공지 API 응답 원본:", response.data);
-    const raw = response.data.result?.notices ?? [];
-
-    return raw.map((n: any) => ({
-      id: n._id,
-      content: n.content,
-      createdAt: n.createdAt,
-    }));
-  } catch (error) {
-    console.error("공지 API 실패:", error);
-    throw error;
-  }
+  const response = await axiosInstance.get('/notices');
+  return response.data.result.notices ?? [];
 };
+
+// export const fetchNotices = async () => {
+//   try {
+//     const response = await axiosInstance.get("/notices");
+//     console.log("공지 API 응답 원본:", response.data);
+//     const raw = response.data.result?.notices ?? [];
+
+//     return raw.map((n: any) => ({
+//       id: n._id,
+//       content: n.content,
+//       createdAt: n.createdAt,
+//     }));
+//   } catch (error) {
+//     console.error("공지 API 실패:", error);
+//     throw error;
+//   }
+// };
 
 // 공지 생성
 export const createNotice = async (content : string) => {
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,12 +6,12 @@ import { useNotices } from "../hooks/useNotice";
 import { useAlarm } from "../hooks/useAlarm";
 
 const Dashboard = () => {
-  const dummyNotices = [
-  { id: "1", content: "금일 23:59 서버 점검 예정입니다", createdAt: "2025-06-03" },
-  { id: "2", content: "신규 기능 배포 안내", createdAt: "2025-06-01" },
-  { id: "3", content: "구름모아 팀 프로젝트 마감일 확인부탁드립니다", createdAt: "2025-06-01" },
-  { id: "4", content: "07/31 단합운동회 예정", createdAt: "2025-06-01" }
-  ];
+  // const dummyNotices = [
+  // { id: "1", content: "금일 23:59 서버 점검 예정입니다", createdAt: "2025-06-03" },
+  // { id: "2", content: "신규 기능 배포 안내", createdAt: "2025-06-01" },
+  // { id: "3", content: "구름모아 팀 프로젝트 마감일 확인부탁드립니다", createdAt: "2025-06-01" },
+  // { id: "4", content: "07/31 단합운동회 예정", createdAt: "2025-06-01" }
+  // ];
   const { gradient } = getGradientByTime();
   const { data: notices = [] } = useNotices();
   useAlarm(true);
@@ -29,8 +29,8 @@ const Dashboard = () => {
       {/*콘텐츠 레이어*/}
       <div className="relative z-10 w-full max-w-[1344px] px-8 pt-[60px]">
         <DashboardHeader />
-        {/* <Notice notices={notices}/> */}
-        <Notice notices={dummyNotices} />
+        <Notice notices={notices}/>
+        {/* <Notice notices={dummyNotices} /> */}
         <DashboardContents notices={notices} />
       </div>
     </div>
